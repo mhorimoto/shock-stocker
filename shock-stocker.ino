0ea8741b64fae8ff11781b0521aa9b64368c8cd6
Masafumi Horimoto on atom3 <horimoto@holly-linux.com>
2020-10-07 18:54:10 +0900

    V1.10 差分をシリアル出力

--- shock-stocker.ino
+++ shock-stocker.ino
@@ -17,20 +17,23 @@
 #define DCHGZ  4  // Discharge Relay for Z axis
 
 Adafruit_ADS1115 ads; // ADS1115 make instance  I2C Address 0x48
+int16_t pwx,pwy,pwz;  // The past value  from ADS 
 
 void setup(void) {
   Serial.begin(115200);
-  Serial.println("SHOCK STOCKER VERSION 1.00");
-  Serial.println("3 SECONDS WAITING");
+  Serial.println("SHOCK STOCKER VERSION 1.10");
+  Serial.println("1 SECONDS WAITING");
   pinMode(DCHGX,OUTPUT);
   pinMode(DCHGY,OUTPUT);
   pinMode(DCHGZ,OUTPUT);
   digitalWrite(DCHGX,HIGH);
-  digitalWrite(DCHGY,HIGH);
-  digitalWrite(DCHGZ,HIGH);
-  delay(3000);
+  delay(300);
   digitalWrite(DCHGX,LOW);
+  digitalWrite(DCHGY,HIGH);
+  delay(300);
   digitalWrite(DCHGY,LOW);
+  digitalWrite(DCHGZ,HIGH);
+  delay(300);
   digitalWrite(DCHGZ,LOW);
   Serial.println("FLUSH CAPACITORS");
   /* ----- Setting of ADS1115 AD converter ----- */
@@ -38,6 +41,12 @@ void setup(void) {
   ads.begin();
   Serial.println("ADS1115 INITIALIZED");
   Serial.end();
+  pwx = ads.readADC_SingleEnded(0);
+  if (pwx<0) pwx=0;
+  pwy = ads.readADC_SingleEnded(1);
+  if (pwy<0) pwy=0;
+  pwz = ads.readADC_SingleEnded(2);
+  if (pwz<0) pwz=0;
 }
 
 #define LIMITV  30000  // 0x7530 91% for Fullscale
@@ -45,6 +54,7 @@ void setup(void) {
 void loop(void) {
   static int i=0;
   int16_t wx,wy,wz; // The direct value read from ADS 
+  int16_t dwx,dwy,dwz; // The different value 
   boolean wxf,wyf,wzf;
   char txt[80];
   
@@ -52,6 +62,13 @@ void loop(void) {
   wx = ads.readADC_SingleEnded(0);
   wy = ads.readADC_SingleEnded(1);
   wz = ads.readADC_SingleEnded(2);
+  dwx = wx - pwx;  pwx = wx;
+  if (dwx<0) dwx = 0;
+  dwy = wy - pwy;  pwy = wy;
+  if (dwy<0) dwy = 0;
+  dwz = wz - pwz;  pwz = wz;
+  if (dwz<0) dwz = 0;
+  
   if (wx>LIMITV) {
     wxf = true;
   } else {
@@ -73,24 +90,24 @@ void loop(void) {
     Serial.print(txt);
     if (wxf) {
       digitalWrite(DCHGX,HIGH);
-      delay(2000);
+      delay(200);
       digitalWrite(DCHGX,LOW);
       Serial.println("FL-X");
     }
     if (wyf) {
       digitalWrite(DCHGY,HIGH);
-      delay(2000);
+      delay(200);
       digitalWrite(DCHGY,LOW);
       Serial.println("FL-Y");
     }
     if (wzf) {
       digitalWrite(DCHGZ,HIGH);
-      delay(2000);
+      delay(200);
       digitalWrite(DCHGZ,LOW);
       Serial.println("FL-Z");
     }
   }
-  sprintf(txt,"DT,%d,%d,%d,%d",i,wx,wy,wz);
+  sprintf(txt,"DT,%d,%d,%d,%d,%d,%d,%d",i,dwx,dwy,dwz,wx,wy,wz);
   Serial.println(txt);
   Serial.end();
   i++;
